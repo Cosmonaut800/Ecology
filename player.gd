7a8ad02a1fc4f2eb01d7bf5076bbba91099dac8d
Sean Miller <seancm800@gmail.com>
2023-09-20 22:36:36 -0400

    Initial commit

---
+++ player.gd
@@ -0,0 +1,60 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const ACCEL = 25.0
+const DECEL = 25.0
+const JUMP_VELOCITY = 4.5
+
+var mouse_sensitivity := 0.001
+var yaw_input := 0.0
+var pitch_input := 0.0
+
+@onready var yaw := $YawPivot
+@onready var pitch := $YawPivot/PitchPivot
+@onready var graphics := $Graphics
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (yaw.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction.y = 0.0
+	if direction:
+		velocity.x = move_toward(velocity.x, SPEED * direction.x, ACCEL * delta)
+		velocity.z = move_toward(velocity.z, SPEED * direction.z, ACCEL * delta)
+		graphics.look_at(position + velocity)
+	else:
+		velocity.x = move_toward(velocity.x, 0, DECEL * delta)
+		velocity.z = move_toward(velocity.z, 0, DECEL * delta)
+
+	move_and_slide()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	yaw.rotate_y(yaw_input)
+	pitch.rotate_x(pitch_input)
+	pitch.rotation.x = clamp(pitch.rotation.x, -1.5, 1.5)
+	yaw_input = 0.0
+	pitch_input = 0.0
+
+func _unhandled_input(event):
+		if event is InputEventMouseMotion:
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+				yaw_input = -event.relative.x * mouse_sensitivity
+				pitch_input = -event.relative.y * mouse_sensitivity
