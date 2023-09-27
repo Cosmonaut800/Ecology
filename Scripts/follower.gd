42335c2386e7ea254afe1281c04a2e53e72763e2
Sean Miller <seancm800@gmail.com>
2023-09-27 02:40:26 -0400

    Fixed follower movement

--- Scripts/follower.gd
+++ Scripts/follower.gd
@@ -8,17 +8,26 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var vertical_velocity := 0.0
 
 func _physics_process(delta):
-	var target_pos = leader.global_position
+	var target_pos = Vector3(leader.global_position.x, position.y, leader.global_position.z)
 	position = position.move_toward(target_pos, 5.0 * delta)
 	target_pos = ray.get_collision_point() + 0.25 * Vector3.UP
-	if (position - target_pos).length() > 0.01 and target_pos.y < position.y:
+	if target_pos.y < position.y:
 		vertical_velocity -= gravity * delta
 		position.y += vertical_velocity * delta
-		if position.y < target_pos.y: position.y = target_pos.y
+		if target_pos.y > position.y:
+			position.y = target_pos.y
+			vertical_velocity = -1.0
+	elif abs(position.y - target_pos.y) < 0.01:
+		position.y = target_pos.y
+		vertical_velocity = -1.0
+	elif target_pos.y > position.y:
+		vertical_velocity = max(10.0, 10.0 * (target_pos.y - position.y))
+		position.y += vertical_velocity * delta
+		vertical_velocity = -1.0
+		if target_pos.y < position.y:
+			position.y = target_pos.y
 	else:
-		vertical_velocity = min(2.0, 0.2 * (target_pos.y - position.y))
-		position.y += vertical_velocity
-		if (position.y > target_pos.y): position.y = target_pos.y
+		print("target position is somehow not in 3D space")
 
 func _process(_delta):
 	basis = player.basis
