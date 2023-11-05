c5c0c9dc7eea8661841ecc1e4d56c8ee536907ef
Sean Miller <seancm800@gmail.com>
2023-11-04 21:26:07 -0400

    Terrain changes

--- Scripts/heightmap_collision.gd
+++ Scripts/heightmap_collision.gd
@@ -1,15 +1,22 @@
 @tool
 extends EditorScript
 
+const ROWS := 4
+const COLUMNS := 4
+const TARGET_COORD := Vector2i(0, 2)
+
 var heightmap = Image.new()
 var height_intensity := 10.0 # /!\ Make sure this scale matches the shader /!\ 
-var collision_shape = get_scene().get_node("./Ground4/StaticBody3D/HeightmapCollision") # /!\ Pick the correct CollisionShape3D /!\
+var collision_shape = get_scene().get_node("./Ground/BorderGround12/StaticBody3D/HeightmapCollision") # /!\ Pick the correct CollisionShape3D /!\
 
 
 func _run():
 	print("Updating heightmap collision")
-	heightmap.load("res://Materials/ecology_heightmap4.png") # /!\ Put the heightmap you want to use here /!\
+	heightmap.load("res://Materials/Textures/ecology_heightmap_extended.png") # /!\ Put the heightmap you want to use here /!\
 	heightmap.convert(Image.FORMAT_RF)
+	
+	collision_shape.shape.map_width = 130
+	collision_shape.shape.map_depth = 130
 	
 	assign_map_data()
 
@@ -31,6 +38,6 @@ func assign_map_data():
 			x = clamp(x, 0, heightmap.get_width() - 1)
 			y = clamp(y, 0, heightmap.get_height() - 1)
 			
-			collision_shape.shape.map_data[k] = (height_intensity / collision_shape.transform.basis.get_scale().y) * heightmap.get_pixel(x, y).r
+			collision_shape.shape.map_data[k] = (height_intensity / collision_shape.transform.basis.get_scale().y) * heightmap.get_pixel(x/COLUMNS+(TARGET_COORD.x*(heightmap.get_width()/COLUMNS)), y/ROWS+(TARGET_COORD.y*(heightmap.get_height()/ROWS))).r
 			k += 1
 	
